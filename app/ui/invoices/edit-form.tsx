f493cda394fd9a7365d15495c0bb4c82cff3786e
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2025-02-10 02:41:29 -0800

    https://nextjs.org/learn/dashboard-app Chapter 12. Mutating data.

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
+import { updateInvoice } from '@/app/lib/actions';
 import {
   CheckIcon,
   ClockIcon,
@@ -17,6 +18,7 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   return (
     <form>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
